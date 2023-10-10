1f7e43072ed3075315f7af4a9233a4e0d8a3440b
jarrodschilling <jarrodschilling@gmail.com>
2023-10-10 09:37:10 -0600

    need to figure out ACTUAL start date for data

--- backtest.py
+++ backtest.py
@@ -1,73 +1,65 @@
 import yfinance as yf
 from matplotlib import pyplot as plt
+import pandas as pd
 import datetime
-
-symbol1="XLE"
-symbol2="OIH"
-symbol3="XOP"
-symbol4="SPY"
-
-symbols = [symbol1, symbol2, symbol3, symbol4]
-
-    
-ma_avg = 20
-days = 100
-# days variable created by asking user how far back in time they want to go, so must make negative
-days = -1 * days
-
-def price_backtest(data, days):
-    price_list = []
-    for i in range(days, -1):
-        closing_price = data['Close'].iloc[i]
-        price_list.append(closing_price)
-
-    return price_list
-
-def ema_20_backtest(data, ema_period, i):
-    data[f'EMA_{ema_period}'] = data['Close'].ewm(span=ema_period, adjust=False).mean()
-    most_recent_20_ema = data[f'EMA_{ema_period}'].iloc[i]
-    return most_recent_20_ema
+from data import indices, sectors
 
 
-# portfolio_ema20 = ma_compute_yf(stocks, "ema20", "today")
-# portfolio_ema20_summary = len(portfolio_ema20) / len(portfolio)
+symbols = sectors
 
 
-def summary_backtest(symbols, period, days):
-    ema_20_dict = {}
-    start_date = "2022-01-01"
-    today = datetime.date.today()
-    tomorrow = today + datetime.timedelta(days=1)
-    end_date = tomorrow.strftime('%Y-%m-%d')
+def api_historical_data(symbols, start_date, end_date):
+    data = yf.download(symbols, start=start_date, end=end_date)
+    return data['Adj Close']
 
-    for symbol in symbols:
-        data = yf.download(symbol, start=start_date, end=end_date)
-        for i in range(days, 0):
-            price = data['Close'].iloc[i]
-            ema_20 = ema_20_backtest(data, period, i)
-            if price > ema_20:
-                ema_20_dict[i] = symbol
-            else:
-                ema_20_dict[i] = None
-    
+# Calculate the EMA for each stock
+def calculate_ema(data, ma_period):
+    return data.ewm(span=ma_period, adjust=False).mean()
 
-    # special_list = []
-    # counter = 0
-    # days = days * -1
-    # for i in range(0, 80):
-    #     for j in range(0, 2):
-    #         if ema_20_dict[j][i] == True:
-    #             print(ema_20_dict[j][i])
-    #             counter += 1
-    #     list_add = counter/len(symbols)
-    #     special_list.append(list_add)
-    
-    return ema_20_dict
+# Calculate the SMA for each stock
+def calculate_sma(data, ma_period):
+    return data.rolling(window=ma_period).mean()
 
-print(summary_backtest(symbols, 20, days))
+# Backtest and calculate the percentage above MA for each date
+def backtest_percentage_above_ema(data, ma_period):
+    ema_data = calculate_sma(data, ma_period)
+    above_ema = data > ema_data
+    percentage_above_ema = (above_ema.sum(axis=1) / len(symbols)) * 100
+    return percentage_above_ema
 
 
-# result = []
-# for x, y in zip(date, ema20):
-#     result.append([x, y])
-# print(result)
+start_date = '2019-01-01'
+end_date = '2020-06-01'
+ma_period = 50
+
+# API call for data
+stock_data = api_historical_data(symbols, start_date, end_date)
+spy_data = api_historical_data('SPY', start_date, end_date)
+
+# Calculate the percentage above EMA for each date
+percentage_above_ema = backtest_percentage_above_ema(stock_data, ma_period)
+
+# Create a DataFrame with the results
+results = pd.DataFrame({'Date': percentage_above_ema.index, 'Percentage Above EMA': percentage_above_ema.values})
+
+plt.style.use('fivethirtyeight')
+
+# Create a new figure with two subplots
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8), sharex=True)
+
+# Plot the percentage above EMA in the first subplot
+ax1.plot(percentage_above_ema.index, percentage_above_ema.values)
+ax1.set_ylabel('Percentage Above EMA')
+ax1.set_title('Percentage of Stocks Above 50-Day SMA')
+
+# Plot the SPY price in the second subplot
+ax2.plot(spy_data.index, spy_data.values)
+ax2.set_ylabel('SPY Price')
+ax2.set_title('SPY Price')
+
+# Customize the plot
+plt.xlabel('Date')
+plt.xticks(rotation=45)
+
+plt.tight_layout()
+plt.show()
